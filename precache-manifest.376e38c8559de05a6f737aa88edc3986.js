8996c70095b81b3413cb9a4511451a44046a017e
Vimal Jaiswal <jaiswalvimal1@gmail.com>
2020-08-17 01:03:00 +0530

    Updates

---
+++ precache-manifest.376e38c8559de05a6f737aa88edc3986.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2278849f1e49d15fa94e73918bb7a0c7",
+    "url": "/VimalJais/index.html"
+  },
+  {
+    "revision": "a1021bc67bb92b0958dd",
+    "url": "/VimalJais/static/css/2.fcacfceb.chunk.css"
+  },
+  {
+    "revision": "f9153c3ea5dfaf9aa7d4",
+    "url": "/VimalJais/static/css/main.5953adbc.chunk.css"
+  },
+  {
+    "revision": "a1021bc67bb92b0958dd",
+    "url": "/VimalJais/static/js/2.1d119bb2.chunk.js"
+  },
+  {
+    "revision": "3da521dbb4874350fb1d5ccbd1bab881",
+    "url": "/VimalJais/static/js/2.1d119bb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9153c3ea5dfaf9aa7d4",
+    "url": "/VimalJais/static/js/main.7175a958.chunk.js"
+  },
+  {
+    "revision": "a153d9c407176df5ab73",
+    "url": "/VimalJais/static/js/runtime-main.4b9f6d5b.js"
+  },
+  {
+    "revision": "46fe501199db20b7d8bfeed6ddc870f6",
+    "url": "/VimalJais/static/media/Amusik.46fe5011.jpg"
+  },
+  {
+    "revision": "bc83a86bace9f540ab6fb7325ad5052f",
+    "url": "/VimalJais/static/media/SmartBrain.bc83a86b.jpg"
+  },
+  {
+    "revision": "11eee562c47e913915376f7a6066859c",
+    "url": "/VimalJais/static/media/background.11eee562.jpg"
+  },
+  {
+    "revision": "a354cdfa9b84938d92de3bd872637784",
+    "url": "/VimalJais/static/media/bootstrap.a354cdfa.png"
+  },
+  {
+    "revision": "0f2ae466d5602a17ce8802a7822e280a",
+    "url": "/VimalJais/static/media/css3.0f2ae466.png"
+  },
+  {
+    "revision": "cbab6217605e912438f26350915ffebe",
+    "url": "/VimalJais/static/media/git.cbab6217.png"
+  },
+  {
+    "revision": "2b3984f7450cd9dbaec2966476d62f6b",
+    "url": "/VimalJais/static/media/js.2b3984f7.png"
+  },
+  {
+    "revision": "8d71067cf13dd636889877bf0de38e73",
+    "url": "/VimalJais/static/media/node.8d71067c.png"
+  },
+  {
+    "revision": "ff3a467a57dd3a644765259759b9f828",
+    "url": "/VimalJais/static/media/postgres.ff3a467a.png"
+  },
+  {
+    "revision": "8babec4be0d2e86a45a1c517483b7689",
+    "url": "/VimalJais/static/media/react.8babec4b.png"
+  }
+]);
